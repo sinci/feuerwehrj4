e5f335b2b5afafac18c77f2154e3c95af1d2f80a
sinci <office@sinci.at>
2022-03-13 01:20:13 +0100

    update ini

--- html/mod_login/default_logout.php
+++ html/mod_login/default_logout.php
@@ -34,7 +34,7 @@ HTMLHelper::_('behavior.keepalive');
 	</ul>
 <?php endif; ?>
 	<div class="mod-login-logout__button logout-button">
-		<button type="submit" name="Submit" class="btn btn-primary"><?php echo Text::_('JLOGOUT'); ?></button>
+		<button type="submit" name="Submit" class="btn btn-primary uk-button uk-button-secondary"><?php echo Text::_('JLOGOUT'); ?></button>
 		<input type="hidden" name="option" value="com_users">
 		<input type="hidden" name="task" value="user.logout">
 		<input type="hidden" name="return" value="<?php echo $return; ?>">
